09c2750fc0f02c888a63cc93eccbce58f530eb16
lcampaci <139291659+lcampaci@users.noreply.github.com>
2026-05-22 10:58:06 -0300

    Simplify service worker caching and fetch handling
    Refactor service worker to simplify caching logic and remove unused cache array.

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,13 @@
-onst CACHE_NAME = "lista-de-compras-v1";
+const CACHE_NAME = "lista-compras-v1";
 
-const urlsToCache = [
-    "./",
-    "./index.html",
-    "./manifest.json"
-];
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
 
-self.addEventListener("install", event => {
-
-    event.waitUntil(
-
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
-
-    event.respondWith(
-
-        caches.match(event.request)
-            .then(response => {
-
-                return response || fetch(event.request);
-            })
-    );
+  event.respondWith(fetch(event.request));
 });
